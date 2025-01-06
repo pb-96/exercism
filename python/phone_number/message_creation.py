3bce091077e057fc90d8e73f9a5e64b8359acd61
pb-96 <philip.byrne@faculty.ai>
2025-01-06 15:30:06 +0000

    adding all potential message creation

---
+++ python/phone_number/message_creation.py
@@ -0,0 +1,31 @@
+from typing import List
+
+COMBINATIONS = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz"
+}
+
+
+def generate_combinations(numbers: List[int]) -> List[str]:    
+    combinations = [char for char in COMBINATIONS.get(numbers.pop())]
+    for number in numbers:
+        next_combinations = COMBINATIONS.get(number)
+        new_combinations = []
+        for char in next_combinations:
+            for to_combine in combinations:
+                new_combinations.append(char + to_combine)
+        combinations = new_combinations
+    return combinations
+
+def get_potential_messages(numbers_typed: str):
+    if not numbers_typed:
+        return []
+    numbers = [*map(int, numbers_typed)]
+    return generate_combinations(numbers)
+
---
+++ python/phone_number/message_creation_test.py
@@ -0,0 +1,16 @@
+import unittest
+from message_creation import get_potential_messages
+
+class TestMessageCreation(unittest.TestCase):
+    def testBasicCase(self):
+        _input = "23"
+        expected = ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']
+        self.assertEqual(get_potential_messages(_input), expected)
+    def testSingleString(self):
+        _input = "2"
+        expected = ['a', 'b', 'c']
+        self.assertEqual(get_potential_messages(_input), expected)
+    def testEmptyCase(self):
+        _input = ""
+        expected = []
+        self.assertEqual(get_potential_messages(_input), expected)
